2591de1fb38e8d9e113efe6758252f7d0ed04f9b
jxch <15553598935@163.com>
2022-05-22 10:11:39 +0800

    log

--- dc/__init__.py
+++ dc/__init__.py
@@ -2,10 +2,12 @@ from . import stock_daily
 
 
 def daily():
+    print("daily job start",  flush=True)
     stock_daily.stock_daily_job()
     stock_daily.stock_daily_kline_job()
 
 
 def init():
+    print("init start",  flush=True)
     stock_daily.stock_daily_job()
     stock_daily.stock_daily_kline_init()
