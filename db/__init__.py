573ec95ef049cea4f99d9d33978bf6feb31e6ba2
jxch <15553598935@163.com>
2022-05-25 17:24:40 +0800

    加载flask上下文

--- db/__init__.py
+++ db/__init__.py
@@ -1,9 +1,6 @@
 from sqlalchemy import create_engine
-from flask import Flask, current_app
-
-app = Flask(__name__)
+from app import app
 
 
 def engine_capital():
-    with app.app_context():
-        return create_engine(current_app.config.get('DB_CAPITAL'), encoding='utf8', pool_timeout=1000)
+    return create_engine(app.config.get('DB_CAPITAL'), encoding='utf8', pool_timeout=1000)
